c48b20beb34cf28eede93e7c5c8f011d72c1aa70
Topher Fangio <fangiotophia@gmail.com>
2010-12-02 13:11:20 -0600

    Initial working fakecas

--- fakecas.rb
+++ fakecas.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
-get '/login' do ""
-  "Hello World!"
+get '/login' do
+  redirect "#{params['service']}&ticket=1234567890"
+end
+
+get '/logout' do
+  redirect "#{params['service']}"
+end
+
+get '/validate' do
+  if params['ticket'] == '1234567890' and not params['pleaseFail']
+    "yes\n#{params['username']}"
+  else
+    "no"
+  end
 end
